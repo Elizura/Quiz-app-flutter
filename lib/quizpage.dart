41870dc994fad21aa1f03f1998e929daa4cf086a
Elizura <98797447+Elizura@users.noreply.github.com>
2023-04-04 15:25:25 +0200

    Quiz page is done, evethou there is an issue handling the states

--- lib/quizpage.dart
+++ lib/quizpage.dart
@@ -14,10 +14,12 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int index = 0;
+  final questions = sample_data;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kBackgroundColor,
+      backgroundColor: Colors.yellow,
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.all(10),
@@ -26,20 +28,34 @@ class _QuizPageState extends State<QuizPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text(
+                Text(
+                    // ignore: prefer_const_constructors
                     style: TextStyle(
                         color: kTextColor,
                         fontSize: 22,
                         fontWeight: FontWeight.bold),
-                    'Question 1/4'),
+                    'Question ${index + 1}'),
                 // Spacer(),
                 Row(
                   children: [
                     IconButton(
-                        onPressed: () => {}, icon: Icon(Icons.arrow_back_ios)),
-                    const Text('1/4'),
+                        onPressed: index == 0
+                            ? null
+                            : () {
+                                setState(() {
+                                  index--;
+                                });
+                              },
+                        icon: Icon(Icons.arrow_back_ios)),
+                    Text('${index + 1}/${questions.length}'),
                     IconButton(
-                        onPressed: () => {},
+                        onPressed: index == questions.length - 1
+                            ? null
+                            : () {
+                                setState(() {
+                                  index++;
+                                });
+                              },
                         icon: const Icon(Icons.arrow_forward_ios)),
                   ],
                 )
@@ -54,7 +70,7 @@ class _QuizPageState extends State<QuizPage> {
             const SizedBox(
               height: 10,
             ),
-            const QuestionCard(),
+            QuestionCard(index: index),
           ],
         ),
       )),
@@ -63,38 +79,146 @@ class _QuizPageState extends State<QuizPage> {
 }
 
 class QuestionCard extends StatefulWidget {
-  const QuestionCard({super.key});
+  QuestionCard({super.key, required this.index});
+  final int index;
 
   @override
   State<QuestionCard> createState() => _QuestionCardState();
 }
 
+enum Choices { choice0, choice1, choice2, choice3 }
+
 class _QuestionCardState extends State<QuestionCard> {
+  @override
+  void initState() {
+    super.initState();
+    String? _yourchoice;
+  }
+
+  String _yourchoice = '';
+
   List? questions = sample_data;
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        tileColor: Colors.white, title: Text(sample_data[0].question));
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Expanded(
+          child: Column(
+            children: [
+              ListTile(
+                tileColor: Colors.white,
+                title: Text(questions?[widget.index].question),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: 4,
+                  itemBuilder: (BuildContext context, int ind) {
+                    return ListTile(
+                      onTap: () {
+                        setState(() {
+                          _yourchoice = 'choices${ind}';
+                        });
+                      },
+                      trailing: Radio(
+                          value: 'choices${ind}',
+                          groupValue: _yourchoice,
+                          onChanged: (value) {
+                            setState(() {
+                              _yourchoice = 'choices${ind}';
+                            });
+                          }),
+                      tileColor: Colors.white,
+                      title: Text(questions?[widget.index].options[ind]),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-  //   return ListView(
-  //     padding: const EdgeInsets.all(8),
-  //     children: <Widget>[
-  //       Container(
-  //         height: 50,
-  //         color: Colors.amber[600],
-  //         child: const Center(child: Text('Entry A')),
-  //       ),
-  //       Container(
-  //         height: 50,
-  //         color: Colors.amber[500],
-  //         child: const Center(child: Text('Entry B')),
-  //       ),
-  //       Container(
-  //         height: 50,
-  //         color: Colors.amber[100],
-  //         child: const Center(child: Text('Entry C')),
-  //       ),
-  //     ],
-  //   );
-  // }
 }
+//     return Expanded(
+//       child: Container(
+//         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+        // decoration: BoxDecoration(
+        //   color: Colors.white,
+        //   borderRadius: BorderRadius.circular(10),
+        // ),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             ListView.builder(
+//               shrinkWrap: true,
+//               itemCount: 1,
+//               itemBuilder: (BuildContext context, int index) {
+//                 return ListTile(
+//                   tileColor: Colors.white,
+//                   title: Text(questions?[widget.index].question),
+//                 );
+//               },
+//             ),
+
+//             //   ListTile(
+//             //       tileColor: Colors.white,
+//             //       title: Text(questions?[widget.index].question)),
+//             //   ListTile(
+//             //     tileColor: Colors.white,
+//             //     trailing:
+//             //         Radio(value: 0, groupValue: 0, onChanged: (onChanged) => {}),
+//             //     title: Text('A. ${questions?[widget.index].options[0]}'),
+//             //   ),
+//             //   ListTile(
+//             //     tileColor: Colors.white,
+//             //     trailing:
+//             //         Radio(value: 0, groupValue: 0, onChanged: (onChanged) => {}),
+//             //     title: Text('A. ${questions?[widget.index].options[0]}'),
+//             //   ),
+//             //   ListTile(
+//             //     tileColor: Colors.white,
+//             //     trailing:
+//             //         Radio(value: 0, groupValue: 0, onChanged: (onChanged) => {}),
+//             //     title: Text('A. ${questions?[widget.index].options[0]}'),
+//             //   ),
+//             //   ListTile(
+//             //     tileColor: Colors.white,
+//             //     trailing:
+//             //         Radio(value: 0, groupValue: 0, onChanged: (onChanged) => {}),
+//             //     title: Text('A. ${questions?[widget.index].options[0]}'),
+//             //   ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//   //   return ListView(
+//   //     padding: const EdgeInsets.all(8),
+//   //     children: <Widget>[
+//   //       Container(
+//   //         height: 50,
+//   //         color: Colors.amber[600],
+//   //         child: const Center(child: Text('Entry A')),
+//   //       ),
+//   //       Container(
+//   //         height: 50,
+//   //         color: Colors.amber[500],
+//   //         child: const Center(child: Text('Entry B')),
+//   //       ),
+//   //       Container(
+//   //         height: 50,
+//   //         color: Colors.amber[100],
+//   //         child: const Center(child: Text('Entry C')),
+//   //       ),
+//   //     ],
+//   //   );
+//   // }
+// }
